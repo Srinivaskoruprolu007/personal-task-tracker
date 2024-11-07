eb5cfa03398d491a63bd945d5a9fa5df11d62007
RAO <korruprolu-srinivasa.rao@capgemini.com>
2024-11-07 14:09:24 +0530

    updated firebase congif

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: "personal-task-tacker.firebaseapp.com",
-  projectId: "personal-task-tacker",
-  storageBucket: "personal-task-tacker.firebasestorage.app",
-  messagingSenderId: "913700237978",
-  appId: "1:913700237978:web:879d7553a8d634c5802523",
-  measurementId: "G-C7D3PGY4DK",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
